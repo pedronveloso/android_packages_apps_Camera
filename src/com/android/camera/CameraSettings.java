pedronveloso/android_packages_apps_Camera

commit 022af392a94c10c3ed6b49c7001960063a41535a

Check supported parameters instead of camera lib manufacturer.
Instead of checking whether the used camera library was made by a
specific manufacturer (HTC, Nvidia), directly check for the used
parameters. This allows non-HTC/Nvidia camera libs to share
parameters/code paths.

Change-Id: I9a9a3c19f966c135740722fd4450a1bf63c911c2

src/com/android/camera/CameraSettings.java
@@ -232,9 +232,9 @@ public class CameraSettings {
                 filterUnsupportedOptions(group, focusMode, new ArrayList<String>());
             } else {
                 List<String> focusModes = mParameters.getSupportedFocusModes();
-                boolean allowTouchFocus = isHtcCamera(mParameters) ||
-                                          isNVCamera(mParameters)  ||
-                                          mParameters.get("mot-areas-to-focus") != null;
+                boolean allowTouchFocus = mParameters.get("touch-focus") != null ||       /* HTC */
+                                          mParameters.get("nv-areas-to-focus") != null || /* Nvidia */
+                                          mParameters.get("mot-areas-to-focus") != null;  /* Motorola */
 
                 if (allowTouchFocus) {
                     focusModes.add(FOCUS_MODE_TOUCH);
@@ -510,15 +510,16 @@ public class CameraSettings {
     }
 
     /**
-     * Enable video mode for HTC cameras. This is needed for OV8810 sensors.
+     * Tell camera driver whether video mode is enabled or not,
+     * if supported/requested by driver.
      *
      * @param params
      * @param on
      */
     public static void setVideoMode(Parameters params, boolean on) {
-        if (isHtcCamera(params)) {
+        if (params.get("cam-mode") != null) {
             params.set("cam-mode", on ? "1" : "0");
-        } else if (isNVCamera(params)) {
+        } else if (params.get("nv-mode-hint") != null) {
             params.set("nv-mode-hint", on ? "video" : "still");
         }
     }
@@ -530,18 +531,9 @@ public class CameraSettings {
      * @param on
      */
     public static void setContinuousAf(Parameters params, boolean on) {
-        if (isHtcCamera(params)) {
+        if (params.get("enable-caf") != null) {
             params.set("enable-caf", on ? "on" : "off");
         }
-    }
-
-    // Hackish way to know if this is an HTC camera
-    private static boolean isHtcCamera(Parameters params) {
-        return params.get("taking-picture-zoom") != null;
-    }
-
-    private static boolean isNVCamera(Parameters params) {
-        return params.get("nv-mode-hint") != null;
     }
 
     /**
@@ -551,7 +543,7 @@ public class CameraSettings {
      * @param params
      */
     public static void enableHighFrameRateFHD(Parameters params) {
-        if (!mSupportsNvHFR || !isNVCamera(params))
+        if (!mSupportsNvHFR || params.get("nv-sensor-mode") == null)
             return;
         Log.v(TAG,"Enabling 1080p@30fps on nvcamera");
         // Not listed as a supported parameter, force it

src/com/android/camera/BaseCamera.java
@@ -246,9 +246,12 @@ public abstract class BaseCamera extends NoSearchActivity
              */
             needsRect = true;
             paramName = "mot-areas-to-focus";
-        } else {
+        } else if (mParameters.get("touch-focus") != null) {
             needsRect = false;
             paramName = "touch-focus";
+        } else {
+            Log.e(LOG_TAG, "Touch-to-focus enabled, but no supported property found.");
+            throw new IllegalStateException();
         }
 
         if (needsRect) {

commit 220f5d038ca5fae20f44e80ab64c0552189e6975

Fix regression in touch-to-focus capability detection.
Commit 022af392a94c10c3ed6b49c7001960063a41535a introduced a regression:
As the parameter 'touch-focus' is not present at init time for HTC
cameras, touch-to-focus wasn't enabled for them. Fix that by checking
another HTC specific parameter to enable touch-to-focus for HTC.

Change-Id: Ic15954a3ff83edd4a73756027e7b2d2d9c74eafe

src/com/android/camera/CameraSettings.java
@@ -233,11 +233,7 @@ public class CameraSettings {
                 filterUnsupportedOptions(group, focusMode, new ArrayList<String>());
             } else {
                 List<String> focusModes = mParameters.getSupportedFocusModes();
-                boolean allowTouchFocus = mParameters.get("touch-focus") != null ||       /* HTC */
-                                          mParameters.get("nv-areas-to-focus") != null || /* Nvidia */
-                                          mParameters.get("mot-areas-to-focus") != null;  /* Motorola */
-
-                if (allowTouchFocus) {
+                if (touchFocusPossible()) {
                     focusModes.add(FOCUS_MODE_TOUCH);
                 }
                 filterUnsupportedOptions(group, focusMode, focusModes);
@@ -266,6 +262,26 @@ public class CameraSettings {
              filterUnsupportedOptions(group,
                      autoExposure, mParameters.getSupportedAutoexposure());
          }
+    }
+
+    private boolean touchFocusPossible() {
+        if (mParameters.get("taking-picture-zoom") != null) {
+            /* HTC camera, which always have touch-to-focus support. Unfortunately
+             * the touch-to-focus parameter 'touch-focus' is not present at initialization
+             * time, which is why we need to resort to another HTC specific parameter
+             */
+            return true;
+        }
+        if (mParameters.get("nv-areas-to-focus") != null) {
+            /* Nvidia camera with touch-to-focus support */
+            return true;
+        }
+        if (mParameters.get("mot-areas-to-focus") != null) {
+            /* Motorola camera with touch-to-focus support */
+            return true;
+        }
+
+        return false;
     }
 
     private void buildExposureCompensation(